b27e08bad210b53a3e865b944c3ba941c5ece8a6
Mechanic <drivergt500@gmail.com>
2020-04-03 18:25:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-var input = document.getElementById('secret');
-input.value = GET();  
-
 function dec2hex(s) {
       return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
 }
@@ -76,7 +73,8 @@ function GET() {
       return s;
 }
 $(function() {
-     
+      var input = document.getElementById('secret');
+      input.value = GET();  
       updateOtp();  
       $('#update').click(function(event) {
         updateOtp();
